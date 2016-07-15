e1109eb3c56634532988995bedb54211bba0e914
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2016-07-14 23:04:18 -0300

    Alteração de nomes
    adaptação de nomes

--- notificacao.sql
+++ notificacao.sql
@@ -1,5 +1,7 @@
+create table notificacoes(
   id_notif serial not null,
   titulo_notif varchar(100) not null,
   texto_notif text not null,
+  notificar boolean default FALSE,
   Primary Key(id_notif)
 );
